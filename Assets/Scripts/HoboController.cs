075f59e2b0eb53f8401bea71a0cbcf168e2cb6ae
hmoss333 <hdamoss@gmail.com>
2018-04-06 14:45:14 -0400

    Update 4/6/2018
    -Added initialize function for hobo script
    -Reading "space" input to re-roll stats

--- Assets/Scripts/HoboController.cs
+++ Assets/Scripts/HoboController.cs
@@ -8,29 +8,36 @@ public class HoboController : MonoBehaviour {
     public int lifeSpan;
     public int incomeValue;
     int maxIncome = 5;
-    
+
     public enum Addictions { weed, heroin, cocain, bathsalts }
     public List<Addictions> addictions;
 
     public enum Skills { perform , rant, harass }
     public List<Skills> skills;
-    
-    // Use this for initialization
-	void Start () {
+
+    void Initialize()
+    {
         lifeSpan = Random.Range(3, 11);
         incomeValue = Random.Range(1, maxIncome);
 
         addictions = RandomizeAddictions(addictions);
         skills = RandomizeSkills(skills);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Initialize();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown("space"))
+            Initialize();
 	}
 
     List<Addictions> RandomizeAddictions(List<Addictions> addictionList)
     {
+        addictionList.Clear();
         int randAmount = Random.Range(1, 3);
 
         for (int i = 0; i < randAmount; i++)
@@ -48,6 +55,7 @@ public class HoboController : MonoBehaviour {
 
     List<Skills> RandomizeSkills(List<Skills> skillList)
     {
+        skillList.Clear();
         int randAmount = Random.Range(1, 3);
 
         for (int i = 0; i < randAmount; i++)
